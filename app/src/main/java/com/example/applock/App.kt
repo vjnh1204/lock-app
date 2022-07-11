24bac5dd450e9ab1eab6b12a0374e6e2e717e12c
VinhNguyen <ntvtht1204@gmail.com>
2022-07-11 16:45:22 +0700

    first commit

---
+++ app/src/main/java/com/example/applock/App.kt
@@ -0,0 +1,28 @@
+package com.example.applock
+
+import android.app.Application
+import android.content.Context
+import io.paperdb.Paper
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Paper.init(this)
+        instance= this
+    }
+    fun setHaveCode(passwordKey: String){
+        val pref = getSharedPreferences(PREFS, Context.MODE_PRIVATE).edit()
+        pref.putString(HAVE_PASSWORD,passwordKey)
+        pref.apply()
+    }
+    fun getHaveCode(): String?{
+        val pref = getSharedPreferences(PREFS, Context.MODE_PRIVATE)
+        return pref.getString(HAVE_PASSWORD,"N")
+    }
+    companion object{
+        var instance: App? = null
+        const val PREFS: String = "SHARED_PREFS"
+        const val HAVE_PASSWORD: String = "HAVE_PASSWORD"
+    }
+
+}
---
+++ app/src/main/java/com/example/applock/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.applock
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.res.ColorStateList
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+}
